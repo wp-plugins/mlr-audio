0700cee81aa22814757d176d74723651e4098a8a
ersatzpole <ersatzpole@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-09 00:59:35 +0000

    Fixed error on uploader.
    git-svn-id: https://plugins.svn.wordpress.org/mlr-audio/trunk@1085232 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/ml.uploader.js
+++ js/ml.uploader.js
@@ -1,59 +1,59 @@
-jQuery(document).ready(function($){
-  var _custom_media = true,
-      _orig_send_attachment = wp.media.editor.send.attachment;
-
-  $('#mp3_upload_button').click(function(e) {
-    var send_attachment_bkp = wp.media.editor.send.attachment;
-    var button = $(this);
-    var id = button.attr('id').replace('mp3_upload_button', 'mp3_url_meta_box_text');
-    _custom_media = true;
-    wp.media.editor.send.attachment = function(props, attachment){
-      if ( _custom_media ) {
-        $("#"+id).val(attachment.url);
-      } else {
-        return _orig_send_attachment.apply( this, [props, attachment] );
-      };
-    }
-
-    wp.media.editor.open(button);
-    return false;
-  });
-  
-  $('#ogg_upload_button').click(function(e) {
-    var send_attachment_bkp = wp.media.editor.send.attachment;
-    var button = $(this);
-    var id = button.attr('id').replace('ogg_upload_button', 'ogg_url_meta_box_text');
-    _custom_media = true;
-    wp.media.editor.send.attachment = function(props, attachment){
-      if ( _custom_media ) {
-        $("#"+id).val(attachment.url);
-      } else {
-        return _orig_send_attachment.apply( this, [props, attachment] );
-      };
-    }
-
-    wp.media.editor.open(button);
-    return false;
-  });
-  
-  $('#wav_upload_button').click(function(e) {
-    var send_attachment_bkp = wp.media.editor.send.attachment;
-    var button = $(this);
-    var id = button.attr('id').replace('wav_upload_button', 'wav_url_meta_box_text');
-    _custom_media = true;
-    wp.media.editor.send.attachment = function(props, attachment){
-      if ( _custom_media ) {
-        $("#"+id).val(attachment.url);
-      } else {
-        return _orig_send_attachment.apply( this, [props, attachment] );
-      };
-    }
-
-    wp.media.editor.open(button);
-    return false;
-  });
-
-  $('.add_media').on('click', function(){
-    _custom_media = false;
-  });
+jQuery(document).ready(function($){
+  var _custom_media = true;	if(wp.media) {
+		var _orig_send_attachment = wp.media.editor.send.attachment;	}
+
+  $('#mp3_upload_button').click(function(e) {
+    var send_attachment_bkp = wp.media.editor.send.attachment;
+    var button = $(this);
+    var id = button.attr('id').replace('mp3_upload_button', 'mp3_url_meta_box_text');
+    _custom_media = true;
+    wp.media.editor.send.attachment = function(props, attachment){
+      if ( _custom_media ) {
+        $("#"+id).val(attachment.url);
+      } else {
+        return _orig_send_attachment.apply( this, [props, attachment] );
+      };
+    }
+
+    wp.media.editor.open(button);
+    return false;
+  });
+  
+  $('#ogg_upload_button').click(function(e) {
+    var send_attachment_bkp = wp.media.editor.send.attachment;
+    var button = $(this);
+    var id = button.attr('id').replace('ogg_upload_button', 'ogg_url_meta_box_text');
+    _custom_media = true;
+    wp.media.editor.send.attachment = function(props, attachment){
+      if ( _custom_media ) {
+        $("#"+id).val(attachment.url);
+      } else {
+        return _orig_send_attachment.apply( this, [props, attachment] );
+      };
+    }
+
+    wp.media.editor.open(button);
+    return false;
+  });
+  
+  $('#wav_upload_button').click(function(e) {
+    var send_attachment_bkp = wp.media.editor.send.attachment;
+    var button = $(this);
+    var id = button.attr('id').replace('wav_upload_button', 'wav_url_meta_box_text');
+    _custom_media = true;
+    wp.media.editor.send.attachment = function(props, attachment){
+      if ( _custom_media ) {
+        $("#"+id).val(attachment.url);
+      } else {
+        return _orig_send_attachment.apply( this, [props, attachment] );
+      };
+    }
+
+    wp.media.editor.open(button);
+    return false;
+  });
+
+  $('.add_media').on('click', function(){
+    _custom_media = false;
+  });
 });
